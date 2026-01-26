6088d24f5ccb825a75bb91968268b8eff54a2914
Brit Meek <bmeek1@byu.edu>
2026-01-26 09:54:36 -0700

    Added support.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Mission4;
 
-Console.WriteLine("Hello, World!");
+support sc = new support();
+
+Console.WriteLine("Welcome to Tic-Tac-Toe");
+
+List<string> choicesPlayerOne = new List<string>();
+List<string> choicesPlayerTwo = new List<string>();
+
+Console.WriteLine("Player 1, Enter your choice in the array: ");
+string player1choice = Console.ReadLine();
+if (player1choice.Length != 1)
+{
+    Console.WriteLine("Guess must be equal to one value");
+
+}
+
+Console.WriteLine("Player 2, Enter your choice in the array: ");
+string player2choice = Console.ReadLine();
+
+do 
+{
+    
+}
+while (!CheckWinner);
